a8881af9e7a5ca4f522bf7f19ef33c4678cd44fe
moshe-blox <moshe@blox.io>
2022-12-22 14:52:28 +0200

    fix: serve pprof on same port as slashing-protector

--- cmd/slashing-protector/main.go
+++ cmd/slashing-protector/main.go
@@ -15,7 +15,6 @@ import (
 var CLI struct {
 	DbPath string `env:"DB_PATH" description:"Path to the database directory" default:"/slashing-protector-data"`
 	Addr   string `env:"ADDR" description:"HTTP address to serve slashing-protector on" default:":9369"`
-	Pprof  string `env:"PPROF" description:"HTTP address to serve pprof on"`
 }
 
 func main() {
@@ -32,15 +31,6 @@ func main() {
 		zap.String("db_path", CLI.DbPath),
 		zap.String("addr", CLI.Addr),
 	)
-
-	// Serve pprof over HTTP.
-	if CLI.Pprof != "" {
-		go func() {
-			// Ignore: G114: Use of net/http serve function that has no support for setting timeouts
-			/* #nosec */
-			log.Println(http.ListenAndServe(CLI.Pprof, nil))
-		}()
-	}
 
 	// Create the server and start it.
 	prtc := protector.New(CLI.DbPath)
